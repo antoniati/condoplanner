2bfc76725481993b67e0bc98a1d64ed33c7b8566
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-02 01:11:41 -0300

    Adicionado botão de login com o Google ao componente LoginPage
    Neste commit, o botão de login com o Google foi implementado e incorporado ao componente LoginPage.
    
    Próxima tarefa planejada para este componente:
    - Implementação da sessão de login com email e senha.
    
    Este commit representa a adição do botão de login com o Google à página de login.

---
+++ components/GoogleButton.js
@@ -0,0 +1,18 @@
+import { FcGoogle } from "react-icons/fc";
+
+const GoogleButton = () => {
+    return (
+        <button className="
+            w-72 sm:w-full flex items-center justify-center gap-2 
+            bg-slate-50 p-3 border-2 border-slate-300 rounded 
+            font-bold text-black tracking-wider text-xl 
+            shadow-md hover:shadow-xl hover:border-luminous-blue 
+            hover:tracking-widest transition-all duration-300
+        ">
+            <FcGoogle size={24} />
+            <span> Google </span>
+        </button>
+    );
+};
+
+export default GoogleButton;
--- components/Login/LoginPage.js
+++ components/Login/LoginPage.js
@@ -1,10 +1,15 @@
-import Logo from "../Logo";
+import Logo from "@/components/Logo";
+import GoogleButton from "@/components/GoogleButton";
 
 const LoginPage = () => {
     return (
         <main className="flex">
-            <section className="w-full sm:w-auto min-h-screen p-10 bg-dark-blue text-white">
-                <Logo />
+            <section className="w-full sm:w-auto min-h-screen p-10 bg-dark-blue text-white flex flex-col items-center gap-10">            
+                    <Logo />
+                <div className="w-80 flex flex-col items-center gap-5">
+                    <h2>Faça Login com sua conta</h2>
+                    <GoogleButton />
+                </div>
             </section>
             <section className="hidden sm:flex w-full min-h-screen p-10">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quibusdam esse eligendi est molestias alias tempore sit nulla atque pariatur possimus ad maxime iste qui nemo, facere, consectetur ut aliquid.
