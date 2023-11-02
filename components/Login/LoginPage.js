1a9bf02194a5fcb338d3b2f3c63e2474ad659b8c
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-02 01:54:46 -0300

    Adicionada a sessão de apresentação à página LoginPage
    Neste commit, foi criada e adicionada a sessão de apresentação à página de login (LoginPage). A sessão de apresentação inclui um texto e uma imagem de fundo, que fornecem informações ou atraem os usuários antes de fazerem login.

--- components/Login/LoginPage.js
+++ components/Login/LoginPage.js
@@ -7,12 +7,14 @@ const LoginPage = () => {
         <main className="flex">
             <section className="w-full sm:w-auto min-h-screen p-10 bg-dark-blue text-white flex flex-col items-center gap-10">
                 <Logo />
+
+                {/* Sessão de Login com Google */}
                 <div className="w-80 flex flex-col items-center gap-5">
                     <h2>Faça Login com sua conta</h2>
                     <GoogleButton />
                 </div>
 
-                {/* Login com Email e Senha */}
+                {/* Sessão de Login com Email e Senha */}
                 <div className="w-72 sm:w-80 flex flex-col gap-5">
                     <p className="text-whit-lines">
                         <span></span>
@@ -22,8 +24,16 @@ const LoginPage = () => {
                     <LoginForm />
                 </div>
             </section>
-            <section className="hidden sm:flex w-full min-h-screen p-10">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quibusdam esse eligendi est molestias alias tempore sit nulla atque pariatur possimus ad maxime iste qui nemo, facere, consectetur ut aliquid.
+
+            {/* Sessão de apresentação da página de Login */}
+            <section className="hidden sm:flex flex-col gap-5 w-full p-10 min-h-screen bg-none sm:bg-[url('/images/bkg-login.png')] bg-no-repeat bg-right-bottom bg-[length:470px_450px] xl:bg-[length:530px_500px] transition-all duration-300">
+                <h1 className="text-3xl text-dark-blue">
+                    Software para Gestão de Condomínios
+                </h1>
+                <p className="text-xl">
+                    Tecnologia que traz Inovação e eficiência para<br></br>
+                    a administração de condomínios
+                </p>
             </section>
         </main>
     );
