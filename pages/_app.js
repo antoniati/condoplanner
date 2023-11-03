56e2d8717decd0d90054be6258e50b330c41f820
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-03 19:53:03 -0300

    fix: Removido Session Provider do NextAuth
    Neste commit foi feito a remoção do SessionProvider do NextAuth, pois a implementação do nextauth será feita posteriormente à autenticação de usuário com email e senha.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import { SessionProvider } from "next-auth/react"
 import '@/styles/globals.css'
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
