d160b895a4c6cec52e2c37e913efbb92b9eca4b7
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-04 17:46:50 -0300

    feat: Criado rota API que contêm as configurações globais do NextAuth.js
    Neste commit foi implementado a rota API padrão do NetAuth, ela é responsável por fornecer as opções de sessão para autenticação, que no caso são o provedor do google GoogleProvider e o adaptador do mongodb MongoDBAdapter() para conexão com banco de dados da aplicação.

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import clientPromise from "@/config/mongodb";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+}
+
+export default NextAuth(authOptions)
