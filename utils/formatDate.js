0315dea14e165ac6c9410b714501e6438948e2d7
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-10 07:53:57 -0300

    refactor: Modificado a função ormatDate() para formatar o fuso horario

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,19 +1,25 @@
 export default function formatDate(dataString) {
-    // Cria um objeto Date a partir da string de data
-    const data = new Date(dataString);
-  
-    // Verifica se a data é válida
-    if (isNaN(data)) {
+  // Cria um objeto Date a partir da string de data
+  const data = new Date(dataString);
+
+  // Verifica se a data é válida
+  if (isNaN(data)) {
       return "-";
-    }
-  
-    // Extraia o dia, mês e ano da data
-    const dia = data.getDate();
-    const mes = data.getMonth() + 1; // Os meses são indexados de 0 a 11, então somamos 1.
-    const ano = data.getFullYear();
-  
-    // Formata a data no formato (dd-mm-yyyy)
-    const formattedDate = `${dia.toString().padStart(2, "0")}-${mes.toString().padStart(2, "0")}-${ano}`;
-  
-    return formattedDate;
   }
+
+  // Configuração do fuso horário para o Brasil
+  const timeZone = 'America/Sao_Paulo';
+
+  // Use a função toLocaleString para formatar a data com o fuso horário desejado
+  const formattedDate = data.toLocaleString('pt-BR', {
+      timeZone,
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+  });
+
+  return formattedDate;
+}
